6053ca7cb580382868ee8924d01c05622a9e0cc1
Andres Miranda <andres.miranda.dev@gmail.com>
2022-06-17 22:52:17 +0900

    after adding the size buttons and giving them some style, even though the style might change later on

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ function makeGrid (rows, cols) {
 };
 
 makeGrid(16, 16);
+
+
 //hovering effect function
 const gridCell = document.querySelectorAll(".grid-cell");
 gridCell.forEach((cell)=>{
@@ -30,4 +32,22 @@ const resetButton = document.getElementById("reset-btn");
 resetButton.addEventListener("click", (e)=>{
     reset();
 })
+
+const sizeButton = document.getElementById("size-btn");
+
+sizeButton.addEventListener("click", (e)=>{
+    const size = document.getElementById("16x16");
+    const size2= document.getElementById("32x32");
+    const size3= document.getElementById("64x64");
+    
+    size.classList.toggle("hide");
+    size.classList.toggle("show");
+    size2.classList.toggle("hide");
+    size2.classList.toggle("show");
+    size3.classList.toggle("hide");
+    size3.classList.toggle("show");
+
+});
+
+const button16 = document.getElementById("")
 
